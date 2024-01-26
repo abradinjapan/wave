4b6b82f373917f7d7ebaf698f761658cd7ad77b6
abradinjapan <abradinjapan@outlook.com>
2024-01-26 16:54:48 -0500

    Removed compiler starting and stopping messages, as well as running code messages.

--- source/main.cpp
+++ source/main.cpp
@@ -7,48 +7,24 @@ void compile_and_run(std::vector<std::string> user_codes, bool debug) {
     runner::program program;
     bool compilation_error = false;
     bool run_time_error = false;
-    basic::buffer input;
-    const char* buffer = "Hello World!";
-
-    // inform user of compilation start
-    std::cout << "----------\n" << "Compiling code." << std::endl;
+    basic::buffer input = basic::buffer(); // blank
 
     // compile program
     program = compiler::compile(user_codes, compilation_error, debug);
 
     // run if no errors occured
     if (compilation_error == false) {
-        // inform user of program start
-        std::cout << "Starting program..." << std::endl;
-
-        // convert test buffer to program input
-        input = basic::buffer((basic::address)buffer, (basic::address)(buffer + strlen(buffer) - 1));
-
         // run code
-        basic::buffer result = runner::run_program(program, input, run_time_error);
-
-        // print result
-        if (result.p_start != 0) {
-            std::cout << "Result buffer: ";
-            result.print();
-            std::cout << std::endl;
-        }
-
-        // inform user of program end
-        std::cout << std::endl << "Ending program..." << std::endl;
+        runner::run_program(program, input, run_time_error);
     } else {
         std::cout << "Code not run, compilation error." << std::endl;
     }
 
-    // close in display area
-    std::cout << "----------" << std::endl;
+    return;
 }
 
 int main(int argc, char** argv) {
     std::vector<std::string> user_codes;
-
-    // inform user of compilation start
-    std::cout << "Starting up compiler." << std::endl;
 
     // get file from arguments
     if (argc > 1) {
